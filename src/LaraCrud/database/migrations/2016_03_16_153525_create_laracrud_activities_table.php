efcaa4f72dfd984e1804842455d52bbba8b65854
Adrian Luna Rojas <adrian.luna.rojas@gmail.com>
2016-03-17 14:15:08 -0400

    Fix Migration class name

--- src/LaraCrud/database/migrations/2016_03_16_153525_create_laracrud_activities_table.php
+++ src/LaraCrud/database/migrations/2016_03_16_153525_create_laracrud_activities_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLaraCrudActivitiesTable extends Migration
+class CreateLaracrudActivitiesTable extends Migration
 {
     /**
      * Run the migrations.
